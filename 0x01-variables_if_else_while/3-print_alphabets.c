2faca0db2d05b8eb9de18eaf5ad93d5e2448ea1e
akram <akram.mo.gb@gmail.com>
2023-03-17 05:47:22 -0700

    Update task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - print the alphabet using putchar method
+* in lower and uppercase
+* putchar - takes one argument and prints it to the
+* console
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+    	for(c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
