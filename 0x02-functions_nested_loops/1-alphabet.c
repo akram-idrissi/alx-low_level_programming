0da0a285dc3f63a63e522358eb05fe40c6878c63
akram <akram.mo.gb@gmail.com>
2023-03-21 12:54:44 -0700

    Add task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * main - prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ *
+ */
+int main()
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		++c;
+	}
+
+	return (0);
+}
