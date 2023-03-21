7b139974c9e4bc617a9d5fdb296894d37e83ecc1
akram <akram.mo.gb@gmail.com>
2023-03-21 12:20:23 -0700

    Add task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - print the string "_putchar"
@@ -7,6 +8,14 @@
  */
 int main(void)
 {
-	write(0, "_putchar\n", 10);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
