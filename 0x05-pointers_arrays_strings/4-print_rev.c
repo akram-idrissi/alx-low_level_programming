782795a2ddaff601329a07132e673566773e8f68
akram <akram.mo.gb@gmail.com>
2023-03-27 11:05:53 -0700

    Add task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse followed by \n
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	int length = strlen(s);
+
+	for (i = length; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
