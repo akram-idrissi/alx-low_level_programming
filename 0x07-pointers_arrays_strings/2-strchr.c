71b7eb6e9c435ae055df3d8b8e2a2b22a8c1ef0d
akram <akram.mo.gb@gmail.com>
2023-04-03 11:51:29 -0700

    Add task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
+	int i = 0;
 
-	while (*s)
+	for (; s[i] >= '\0'; i++)
 	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	if (c == '\0')
-		return (s);
 
-	return (0);
+	return (NULL);
 }
-
